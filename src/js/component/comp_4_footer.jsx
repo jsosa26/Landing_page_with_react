cbafea80937b09b9f36c85a862439bfd5d3b930d
jsosa26 <99664039+jsosa26@users.noreply.github.com>
2023-09-06 23:57:11 +0000

    footer y navbar react

---
+++ src/js/component/comp_4_footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const comp_4_footer = () => {
+    return (
+        <div className="container-fluid bg-dark text-center py-3 text-white">
+            <p>Copyright © my landing page with react 2022</p>
+        </div>
+    )
+}
