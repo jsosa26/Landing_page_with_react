991c24c4c921063c7f6c6a5670d83104c016cb6b
jsosa26 <99664039+jsosa26@users.noreply.github.com>
2023-09-06 23:49:26 +0000

    card lista

---
+++ src/js/component/comp_3_cards.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const comp_3_cards = () => {
+    return (
+        <div className="container">
+        <div className="card col-lg-12 col-lg-3">
+            <img src="https://cdn-icons-png.flaticon.com/512/1995/1995515.png"
+            className="card-img-top"
+            alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore aliquam velit omnis rerum minus distinctio dignissimos quidem beatae alias, aut doloremque incidunt temporibus amet fuga modi, nam voluptatum minima quibusdam?</p>
+                <a href="#" className="btn btn-primary">Find out more</a>
+            </div>
+        </div>
+        </div>
+    )
+};
